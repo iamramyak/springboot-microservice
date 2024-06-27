f8dd443e47356c7a027188a85dfc7b1925348b71
iamramyak <iamramyak7@gmail.com>
2024-06-27 12:09:59 +0530

    springboot microservice

---
+++ src/test/java/javaspringbootmicroservice/springboot_microservice/SpringbootMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package javaspringbootmicroservice.springboot_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
